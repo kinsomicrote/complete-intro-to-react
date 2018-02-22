dd0228acb99cb22af95e1a461df7fdf73550b8c1
kinsomicrote <kinsomicrote.inc@gmail.com>
2018-02-22 14:27:53 +0100

    added BrowserRouter and few components

---
+++ js/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Search = () => (
+  <div className="">
+    <h1>this is a lol search</h1>
+  </div>
+);
+
+export default Search;
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,37 @@
 const path = require('path');
- 
+
 module.exports = {
   context: __dirname,
   entry: './js/ClientApp.jsx',
   devtool: 'cheap-eval-source-map',
   output: {
     path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+  },
+  devServer: {
+    publicPath: '/public/',
+    historyApiFallback: true,
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json']
+    extensions: ['.js', '.jsx', '.json'],
   },
   stats: {
     colors: true,
     reasons: true,
-    chunks: true
+    chunks: true,
   },
   module: {
     rules: [
       {
+        enforce: 'pre',
         test: /\.jsx?$/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
 };
