a6007bccc17fddf2a33b35f303bb993cbb859ade
kinsomicrote <kinsomicrote.inc@gmail.com>
2018-02-23 09:19:42 +0100

    removed construtor

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,13 +3,9 @@ import preload from '../data.json';
 import ShowCard from './ShowCard';
 
 class Search extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      searchTerm: '',
-    };
-  }
+  state = {
+    searchTerm: '',
+  };
   handleSearchTermChange = (event) => {
     this.setState({ searchTerm: event.target.value });
   }
