bd489cc2a146f9bd4b6b1e20d9a127627df29a60
kinsomicrote <kinsomicrote.inc@gmail.com>
2018-03-05 15:24:21 +0100

    added types for flow

--- flow-typed/types.js
+++ flow-typed/types.js
@@ -6,11 +6,20 @@ export type Show = {
   year: string,
   imdbID: string,
   trailer: string,
-  poster: string
+  poster: string,
 };
 
 declare var module: {
   hot: {
-    accept(path: string, callback: () => void): void
-  }
-}
+    accept(path: string, callback: () => void): void,
+  },
+};
+
+declare type ActionType = 'SET_SEARCH_TERM';
+
+declare type ActionT<A: ActionType, P> = {|
+  type: A,
+  payload: P,
+|};
+
+export type Action = ActionT<'SET_SEARCH_TERM', string>;
