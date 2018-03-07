4e4a6c546b17144d461105b4135a515f52e3da11
kinsomicrote <kinsomicrote.inc@gmail.com>
2018-03-07 15:32:08 +0100

    added tests with redux'

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import store from '../store';
 import preload from '../../data.json';
-import { Unwrapped as UnwrappedSearch } from '../Search';
+import Search, { Unwrapped as UnwrappedSearch } from '../Search';
 import ShowCard from '../ShowCard';
+import { setSearchTerm } from '../actionCreators';
 
 test('Search renders correctly', () => {
   const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm="" />);
   expect(component).toMatchSnapshot();
 });
 
-xtest('Search should render correct amount of shows', () => {
+test('Search should render correct amount of shows', () => {
   const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm="" />);
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
-test('Search should render correct amount of shows based on search term', () => {
+test('Search should render correct amount of shows based on search term - without redux', () => {
   const searchWord = 'black';
   const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm={searchWord} />);
   // component.find('input').simulate('change', { target: { value: searchWord } });
@@ -22,4 +26,20 @@ test('Search should render correct amount of shows based on search term', () => 
     show => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0
   ).length;
   expect(component.find(ShowCard).length).toEqual(showCount);
+});
+
+test('Search should render correct amount of shows based on search term - with redux', () => {
+  const searchWord = 'black';
+  store.dispatch(setSearchTerm(searchWord));
+  const component = render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <Search shows={preload.shows} searchTerm={searchWord} />
+      </MemoryRouter>
+    </Provider>
+  );
+  const showCount = preload.shows.filter(
+    show => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0
+  ).length;
+  expect(component.find('.show-card').length).toEqual(showCount);
 });
